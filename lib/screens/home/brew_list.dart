7778944e3adad91f15250774d1348581e9e5a356
Andrea Bonatti <andreabonatti92@gmail.com>
2021-11-10 17:22:01 +0100

    Flutter & Firebase App Tutorial #20 - Listing Brew Data https://www.youtube.com/watch?v=TKM6_MTNGsI&list=PL4cUxeGkcC9j--TKIdkb3ISfRbJeJYQwC&index=20

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -2,6 +2,8 @@ import 'package:brew_crew/models/brew.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'brew_tile.dart';
+
 class BrewList extends StatefulWidget {
   @override
   _BrewListState createState() => _BrewListState();
@@ -24,6 +26,11 @@ class _BrewListState extends State<BrewList> {
       print(brew.strength);
     }
 
-    return Container();
+    return ListView.builder(
+      itemCount: brews.length,
+      itemBuilder: (context, index) {
+        return BrewTile(brew: brews[index]);
+      },
+    );
   }
 }
