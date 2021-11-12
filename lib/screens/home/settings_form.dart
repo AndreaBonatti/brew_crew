64777a8fbd65a4e3837ab5bced944ae3596868a0
Andrea Bonatti <andreabonatti92@gmail.com>
2021-11-12 09:05:33 +0100

    Flutter & Firebase App Tutorial #23 - Sliders https://www.youtube.com/watch?v=9gWYe8xCD5I&list=PL4cUxeGkcC9j--TKIdkb3ISfRbJeJYQwC&index=23

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -11,9 +11,9 @@ class _SettingsFormState extends State<SettingsForm> {
   final List<String> sugars = ['0', '1', '2', '3', '4'];
 
   // form values
-  late String _currentName;
-  late String _currentSugars;
-  late int _currentStrength;
+  String _currentName = 'User';
+  String _currentSugars = '0';
+  int _currentStrength = 100;
 
   @override
   Widget build(BuildContext context) {
@@ -28,8 +28,10 @@ class _SettingsFormState extends State<SettingsForm> {
           const SizedBox(height: 20.0),
           TextFormField(
             decoration: textInputDecoration,
-            validator: (val) => (val != null && val.isEmpty)  ? 'Please enter a name' : null,
+            validator: (val) =>
+                (val != null && val.isEmpty) ? 'Please enter a name' : null,
             onChanged: (val) => setState(() => _currentName = val),
+            initialValue: _currentName ?? 'User',
           ),
           const SizedBox(height: 10.0),
           DropdownButtonFormField<String>(
@@ -40,6 +42,15 @@ class _SettingsFormState extends State<SettingsForm> {
               );
             }).toList(),
             onChanged: (val) => setState(() => _currentSugars = val!),
+            value: _currentSugars ?? '0',
+          ),
+          Slider(
+            value: (_currentStrength ?? 100).toDouble(),
+            activeColor: Colors.brown[_currentStrength ?? 100],
+            min: 100,
+            max: 900,
+            divisions: 8,
+            onChanged: (val) => setState(() => _currentStrength = val.round()),
           ),
           const SizedBox(height: 10.0),
           ElevatedButton(
